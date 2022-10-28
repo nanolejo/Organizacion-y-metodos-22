83fc5f2ab3fa455da33e7022ea2830df91744156
DarianRuiz <103455625+DarianRuiz@users.noreply.github.com>
2022-10-28 15:58:43 -0300

    Update js.js

--- web/js.js
+++ web/js.js
@@ -1,18 +1,14 @@
-// Variables
 const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
 const cuil = document.getElementById("cuil");
 const antiguedad = document.getElementById("antiguedad");
-////////
 const sueldobasico = document.getElementById("sueldobasico");
 const extras50 = document.getElementById("extras50");
 const extras100 = document.getElementById("extras100");
-///////
 const feriados = document.getElementById("feriados");
 const licencia = document.getElementById("licencia");
 const licenciapor = document.getElementById("licenciapor");
 const faltas = document.getElementById("faltas");
-///////
 
 document.addEventListener("keydown", (event) => {
   if (event.keyCode == 13) {
@@ -25,10 +21,8 @@ function generar() {
   let conf = confirm("Esta seguro de los datos ingresados?");
   if (conf == true) {
     const valor_hora = parseInt(sueldobasico.value / 176);
-    //////// Operacion Productividad /////////
+    
     const res_productividad = parseInt(sueldobasico.value / 100) * 10;
-
-    //////// Operacion Presentismo/////////
     if (faltas.value >= 5) {
       var porc_suma = 0; //valor anterior: 25
     } else {
@@ -36,28 +30,19 @@ function generar() {
     }
     const res_presentismo = parseInt(sueldobasico.value / 100) * porc_suma;
 
-    //////// Operacion Antiguedad /////////
     const cuenta_antiguedad =
       parseInt(sueldobasico.value / 100) * antiguedad.value;
     const res_antiguedad = cuenta_antiguedad * antiguedad.value;
 
-    //////// Operacion Feriados /////////
     const res_feriado = feriados.value * valor_hora * 8;
 
-    //////// Operacion licencia /////////
     const res_licenciapor = licenciapor.value * valor_hora / 100 * 8 ;
     const res_licencia = licencia.value * res_licenciapor ;
 
-    /////// Operacion Horas Extra (50) /////////
     const res_horas50 = Number(valor_hora / 2) * extras50.value;
-    // const res_horas50 = horas50cuenta * horas50valor;
 
-    /////// Operacion Horas Extra (100) /////////
     const res_horas100 = Number(valor_hora) * extras100.value;
 
-    /////////// Operacion Faltas ///////////////
-
-    /////////// Operacion Aguinaldo ///////////////
     const res_aguinaldo =
       parseInt(
         Number(res_presentismo) +
@@ -66,7 +51,6 @@ function generar() {
           Number(res_horas100)
       ) / 2;
 
-    /////////// Operacion Vacaciones ///////////////
     const res_vacaciones =
       parseInt(
         Number(sueldobasico.value) +
@@ -74,7 +58,6 @@ function generar() {
           Number(res_productividad)
       ) / 2;
 
-    /////////// Operacion Remuneracion Bruta ///////////////
     const remuneracionbruta =
       Number(sueldobasico.value) +
       Number(res_productividad) +
@@ -83,22 +66,16 @@ function generar() {
       Number(res_horas50) +
       Number(res_horas100);
 
-    /////////// Operacion SIPA ///////////////
     const res_sipa = parseInt(remuneracionbruta / 100) * 11;
 
-    /////////// Operacion INSSJP ////////////////////
     const res_inssjp = parseInt(remuneracionbruta / 100) * 3;
 
-    /////////// Operacion Obra Social ///////////////
     const res_obrasocial = parseInt(remuneracionbruta / 100) * 3;
 
-    /////////// Operacion Total Deducciones ///////////////
     const totaldeducciones =
       Number(res_sipa) + Number(res_inssjp) + Number(res_obrasocial);
 
-    /////////// Operacion Remuneracion Neta ///////////////
     const remuneracion_neta = remuneracionbruta - totaldeducciones;
-    ///////////////////////////////////////////////
 
     const resultText = document.getElementById("blankid");
     resultText.innerHTML = `
